4558bd085ba3d622b20b8737efc299755cea240c
Aron Hedman <aronhedman@gmail.com>
2025-02-02 16:15:39 +0100

    Forts lobbys

--- public/room.js
+++ public/room.js
@@ -6,8 +6,12 @@ const socket = io();
 const users = {};
 
 socket.on('newUserJoined', (socket, username) => {
-    Object.assign(users, {socket, username});
-    document.getElementById('userList').createElement('li').textContent = username;
-    console.log(users)
+    users[socketId] = username;
+    const userList = document.getElementById('userList');
+    const listItem = document.createElement('li');
+    listItem.textContent = username;
+    userList.appendChild(listItem);
+    console.log(users);
 })
 
+    
